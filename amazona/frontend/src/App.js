939244dfdcef0549cd539e0943c5b87216fa7a11
JayaKumar-2003 <jayakumar112234@gmail.com>
2023-02-20 16:59:36 +0530

    20.2.2023 commit

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,15 +1,23 @@
 // import data from './data';
-import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen  from './screens/ProductScreen';
-
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import  {LinkContainer} from 'react-router-bootstrap';
 function App() {
   return (
    <BrowserRouter>
     <div>
       <header>
-        <Link to="/"> Amazona</Link>
+        <Navbar bg ='dark' variant = 'dark'>
+        <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main>
         <Routes>
@@ -18,8 +26,10 @@ function App() {
           <Route path='/product/:slug' element={<ProductScreen></ProductScreen>}>
           </Route>
         </Routes>
-        
       </main>
+      <footer>
+        <div className='text-centre'>All rights reserved</div>
+      </footer>
     </div>
    </BrowserRouter>
       
--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,37 +1,64 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+// import logger from 'use-reducer-logger';
 import '../index.css';
-
+import Product from './Products';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer((reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('api/products');
-      setProducts(result.data);
+      dispatch({type:'FETCH_REQUEST'});
+      try {
+        const result = await axios.get('api/products');
+        dispatch({type:'FETCH_SUCCESS',payload:result.data});
+      }catch(err) {
+          dispatch({type:'FETCH_FAIL',payload:err.message});
+      }
+      
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
   return (
-    <div>
+    <div className='d-flex flex-column site-container'>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.images} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>
-        ))}
+
+        {
+          loading ?(
+            <div>Loading....</div>
+          ): error ? (
+            <div>{error}</div>
+          ) : (
+            <Row> {
+        products.map((product) => (
+          <Col sm={6} md={4} lg={3} className="mb-3">
+          <Product product={product}></Product>
+          </Col>
+        )
+        
+        ) }
+        </Row>)}
       </div>
     </div>
   );
